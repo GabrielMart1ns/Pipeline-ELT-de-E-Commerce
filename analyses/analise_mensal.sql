ca1565ece08664a507152e227ca5cca974336fe5
Gabriel Martins de Souza <gabrielms12.gms@gmail.com>
2026-03-17 14:09:59 +0000

    Criação de análises sobre o pipeline - V2

---
+++ analyses/analise_mensal.sql
@@ -0,0 +1,35 @@
+-- Análise mensal de receita, qtd_produtos e lucro
+WITH
+    calculos AS (
+        SELECT
+            DATE_TRUNC(order_date, MONTH) AS month,
+            TRUNC(SUM(amount), 2) AS revenue,
+            SUM(quantity) AS products_ordered,
+            TRUNC(SUM(profit), 2) AS profit
+        FROM
+            `portfolio-101-417118.portfolio.int_ecommerce`
+        GROUP BY
+            1
+    ),
+    resultados_mensal AS (
+        SELECT
+            month,
+            FORMAT_DATE('%b/%y', month) AS month_year,
+            revenue,
+            products_ordered,
+            profit,
+            ROUND(SAFE_DIVIDE(profit, revenue)*100, 2) AS profit_margin,
+            LAG(revenue) OVER(ORDER BY month) AS previous_month_rev
+        FROM
+            calculos
+    )
+SELECT
+    month_year,
+    revenue,
+    products_ordered,
+    profit,
+    profit_margin,
+    (revenue - previous_month_rev) AS revenue_diff,
+    ROUND((SAFE_DIVIDE(revenue, previous_month_rev)-1)*100, 2) as revenue_mom_growth
+FROM
+    resultados_mensal
